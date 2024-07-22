e0cba1652b22039a4312194ad7084a38b2971970
slobys <66398890+slobys@users.noreply.github.com>
2024-07-22 14:34:35 +0800

    Add files via upload

---
+++ headscale.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+read -p "请输入服务器IP: " SERVER_IP
+read -p "请输入Headscale端口: " HEADSCALE_PORT
+read -p "请输入IP前缀（例如：100.64.0.0）: " IP_PREFIX
+read -p "请输入Derp服务端口: " DERP_PORT
+
+# 更新系统并安装依赖
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y wget unzip
+
+# 重命名并安装Headscale
+mv headscale_0.23.0-alpha12_linux_amd64.deb headscale.deb
+sudo dpkg -i headscale.deb
+
+# 设置Headscale开机自启并启动
+sudo systemctl enable headscale
+sudo systemctl start headscale
+
+# 安装Nginx
+sudo apt install -y nginx
+
+# 配置Nginx
+sudo sed -i '/# Please see \/usr\/share\/doc\/nginx-doc\/examples\/ for more detailed examples./a \
+##\n\
+map $http_upgrade $connection_upgrade {\n\
+    default keep-alive;\n\
+    "websocket" upgrade;\n\
+    "" close;\n\
+}\n\
+server {\n\
+    listen '"$HEADSCALE_PORT"';\n\
+    listen [::]:'"$HEADSCALE_PORT"';\n\
+    server_name '"$SERVER_IP"';\n\
+    location / {\n\
+        proxy_pass http://127.0.0.1:8080;\n\
+        proxy_http_version 1.1;\n\
+        proxy_set_header Upgrade $http_upgrade;\n\
+        proxy_set_header Connection $connection_upgrade;\n\
+        proxy_set_header Host $host;\n\
+        proxy_buffering off;\n\
+        proxy_set_header X-Real-IP $remote_addr;\n\
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\
+        proxy_set_header X-Forwarded-Proto $scheme;\n\
+        add_header Strict-Transport-Security "max-age=15552000; includeSubDomains" always;\n\
+    }\n\
+    location /web {\n\
+        index index.html;\n\
+        alias /var/www/web;\n\
+    }\n\
+}\n\
+server {\n\
+    listen 80;\n\
+    listen [::]:80;\n\
+    server_name 127.0.0.1;\n\
+    root /var/www;\n\
+    index index.html index.htm index.nginx-debian.html;\n\
+    location /d {\n\
+        alias /var/www;\n\
+        autoindex on;\n\
+    }\n\
+    location / {\n\
+        try_files $uri $uri/ =404;\n\
+    }\n\
+}' /etc/nginx/sites-available/default
+
+# 重启Nginx
+sudo systemctl restart nginx
+
+# 解压并安装Headscale UI
+sudo unzip headscale-ui.zip -d /var/www
+
+# 创建 derp.json 文件
+sudo bash -c "cat > /var/www/derp.json << EOF
+{
+    \"Regions\": {
+        \"900\": {
+            \"RegionID\": 900,
+            \"RegionCode\": \"myderp\",
+            \"Nodes\": [
+                {
+                    \"Name\": \"a\",
+                    \"RegionID\": 900,
+                    \"DERPPort\": $DERP_PORT,
+                    \"IPv4\": \"$SERVER_IP\",
+                    \"InsecureForTests\": true
+                }
+            ]
+        }
+    }
+}
+EOF"
+
+# 修改Headscale配置文件
+sudo sed -i "s|^server_url:.*|server_url: http://$SERVER_IP:$HEADSCALE_PORT|" /etc/headscale/config.yaml
+sudo sed -i "s|^\( *\)v4: 100.64.0.0/10|\1v4: $IP_PREFIX/24|" /etc/headscale/config.yaml
+sudo sed -i "s|^\( *\)v6: fd7a:115c:a1e0::/48|#\1v6: fd7a:115c:a1e0::/48|" /etc/headscale/config.yaml
+sudo sed -i "s|^\( *\)- https://controlplane.tailscale.com/derpmap/default|#\1- https://controlplane.tailscale.com/derpmap/default\n\1- http://127.0.0.1/d/derp.json|" /etc/headscale/config.yaml
+
+# 重启服务
+sudo systemctl restart headscale
+sudo systemctl restart nginx
+
+# 生成API密钥
+headscale apikeys create --expiration 9999d
+
+echo "安装完成！请使用生成的API密钥和以下链接登陆Headscale-ui面板："
+echo "http://$SERVER_IP:$HEADSCALE_PORT/web"
+echo "使用以下命令将设备连接到Headscale服务器："
+echo "tailscale up --login-server=http://$SERVER_IP:$HEADSCALE_PORT"
+
---
+++ tailscale.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# 获取用户输入的域名和端口
+read -p "请输入域名: " DOMAIN
+read -p "请输入Derp服务端口: " DERP_PORT
+read -p "请输入HTTP端口: " HTTP_PORT
+# 关闭防火墙功能
+disable_firewall() {
+    # 关闭ufw
+    if command -v ufw > /dev/null 2>&1; then
+        ufw disable
+        echo "ufw 已关闭"
+    fi
+    # 关闭firewalld
+    if command -v firewall-cmd > /dev/null 2>&1; then
+        systemctl stop firewalld
+        systemctl disable firewalld
+        echo "firewalld 已关闭"
+    fi
+    # 关闭iptables
+    if command -v iptables > /dev/null 2>&1; then
+        iptables -F
+        iptables -X
+        iptables -t nat -F
+        iptables -t nat -X
+        iptables -t mangle -F
+        iptables -t mangle -X
+        iptables -t raw -F
+        iptables -t raw -X
+        echo "iptables 规则已清除"
+    fi
+}
+# 关闭所有防火墙
+disable_firewall
+# 更新和升级软件包
+apt update && apt upgrade -y
+# 安装依赖包
+apt install -y wget git openssl curl
+# 下载并安装Go
+GO_VERSION="go1.22.4"   #这个可以替换为最新的版本
+rm -rf /usr/local/go && tar -C /usr/local -xzf ${GO_VERSION}.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
+source /etc/profile
+# 设置Go环境变量
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
+# 安装Derper服务器源码
+go install tailscale.com/cmd/derper@main
+DERPER_PATH=$(go env GOPATH)/pkg/mod/tailscale.com@*/cmd/derper
+# 修改cert.go文件
+CERT_GO_PATH=$(find ${DERPER_PATH} -type f -name "cert.go")
+sed -i '/if hi.ServerName != m.hostname/,+2 s/^/\/\//' ${CERT_GO_PATH}
+# 编译Derper服务器
+cd ${DERPER_PATH}
+go build -o /etc/derp/derper
+# 返回到根目录
+cd ~
+# 生成自签名SSL证书
+mkdir -p /etc/derp
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout /etc/derp/${DOMAIN}.key -out /etc/derp/${DOMAIN}.crt -subj "/CN=${DOMAIN}" -addext "subjectAltName=DNS:${DOMAIN}"
+# 创建systemd服务文件
+cat > /etc/systemd/system/derp.service <<EOF
+[Unit]
+Description=TS Derper
+After=network.target
+Wants=network.target
+[Service]
+User=root
+Restart=always
+ExecStart=/etc/derp/derper -hostname ${DOMAIN} -a :${DERP_PORT} -http-port ${HTTP_PORT} -certmode manual -certdir /etc/derp
+RestartPreventExitStatus=1
+[Install]
+WantedBy=multi-user.target
+EOF
+# 设置Derper服务开机启动并启动服务
+systemctl enable derp
+systemctl start derp
+# 安装Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+# 重新加载systemd服务并重启Derper服务
+systemctl daemon-reload
+systemctl restart derp
+echo "Tailscale和Derper服务器安装配置完成"
